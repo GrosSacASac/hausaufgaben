077c051527d7390b88df28a8b78adab1ef2d964b
Cyril Walle <cyril.walle@protonmail.com>
2025-04-16 18:11:29 +0200

    b
    only diagonals not +
    only direct MAS no discontinuity
    could simplify code a lot

--- adventofcode/2024/4.js
+++ adventofcode/2024/4.js
@@ -12,6 +12,13 @@ const first = order[0];
 const last = order[order.length - 1];
 const relevant = order.join("", "");
 
+const orderb = ["M", "A", "S"];
+const middle = "A";
+const wordLengthb = orderb.length;
+const firstb = orderb[0];
+const lastb = orderb[orderb.length - 1];
+const relevantb = orderb.join("", "");
+
 const countLineSingle = (lineArray) => {
     let total = 0;
     let position = -1;
@@ -113,6 +120,103 @@ const diagonalsInDirectionFromLines = (lines, directionX, directionY) => {
     }
     }
     return diagonales;
+};
+
+const identifyIndiagonalsInDirectionFromLines = (lines, directionX, directionY) => {
+   
+    
+    const coordinates = [];
+    let positionmiddley=0;
+    let positionmiddlex=0;
+
+    const startY = -0.5 * (directionY -1) * (lines.length - 1);
+    const endY = 0.5 * (directionY +1) * (lines.length - 1)
+    for (let i = startY; i !== endY + directionY; i += directionY) {
+        let y = i;
+        let x = -0.5 * (directionX -1) * (lines[0].length - 1);
+        const endX = 0.5 * (directionX +1) * (lines[0].length - 1);
+        const lengthOfThisDiagonale = Math.min(
+            Math.abs(y - endY) + 1,
+            Math.abs(x - endX) + 1,
+        );
+        if (lengthOfThisDiagonale >= wordLengthb) {
+            let position = -1;
+            let i = 0;
+            let c = "";
+            for (let k = 0; k < lengthOfThisDiagonale; k += 1) {
+                c = lines[y][x]
+                if (relevantb.includes(c)) {
+                    if (position + 1 === orderb.indexOf(c)) {
+                        position += 1;
+                        if (c === middle) {
+                            positionmiddlex = x;
+                            positionmiddley = y 
+                        }
+                        if (c === lastb) {
+                            coordinates.push(`${positionmiddley},${positionmiddlex}`);
+                            position = -1;
+                        }
+                    } else if (c === firstb) {
+                        position = 0;
+                    } else {
+                        position = -1;
+                    }
+                } else {
+                    position = -1;
+                }
+
+                y += directionY;
+                x += directionX;
+            }
+        }
+    }
+    {
+        // don't do the main diagonale twice
+    const startX = -0.5 * (directionX -1) * (lines[0].length - 1) + directionX;
+    const endX = 0.5 * (directionX +1) * (lines[0].length - 1);
+    for (let j = startX; j !== endX + directionX; j += directionX) {
+        const diagonale = [];
+        let x = j;
+        let y = -0.5 * (directionY -1) * (lines.length - 1);
+        const endY = 0.5 * (directionY +1) * (lines.length - 1)
+        const lengthOfThisDiagonale = Math.min(
+            Math.abs(y - endY) + 1,
+            Math.abs(x - endX) + 1,
+        );
+        
+        if (lengthOfThisDiagonale >= wordLengthb) {
+            let position = -1;
+            let i = 0;
+            let c = "";
+            for (let k = 0; k < lengthOfThisDiagonale; k += 1) {
+                c = lines[y][x]
+                if (relevantb.includes(c)) {
+                    if (position + 1 === orderb.indexOf(c)) {
+                        position += 1;
+                        if (c === middle) {
+                            positionmiddlex = x;
+                            positionmiddley = y 
+                        }
+                        if (c === lastb) {
+                            coordinates.push(`${positionmiddley},${positionmiddlex}`);
+                            position = -1;
+                        }
+                    } else if (c === firstb) {
+                        position = 0;
+                    } else {
+                        position = -1;
+                    }
+                } else {
+                    position = -1;
+                }
+
+                y += directionY;
+                x += directionX;
+            }
+        }
+    }
+    }
+    return coordinates;
 }
 
 const diagonalsFromLines = (lines) => {
@@ -121,6 +225,15 @@ const diagonalsFromLines = (lines) => {
     );
 };
 
+const countDuplicates = (arrays) => {
+    let totalLengthOriginal = 0;
+    arrays.forEach(array => {
+        totalLengthOriginal += array.length;
+    });
+    
+    const set = new Set([].concat(...arrays));
+    return totalLengthOriginal - set.size;
+};
 
 const input = fs.readFileSync(`${__dirname}/4input.txt`, 'utf-8');
 console.time("Time");
@@ -135,5 +248,14 @@ const total = all.reduce((p, line) => {
     return p + countLineAndReverse(line);
 }, 0);
 
+let a = identifyIndiagonalsInDirectionFromLines(lines, 1, 1);
+let b = identifyIndiagonalsInDirectionFromLines(lines, 1, -1);
+let c = (identifyIndiagonalsInDirectionFromLines(lines, -1, 1));
+let d = (identifyIndiagonalsInDirectionFromLines(lines, -1, -1));
+const xmascount = countDuplicates([a,b,c,d])
 console.timeEnd("Time");
-console.log(total)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(total,xmascount);
